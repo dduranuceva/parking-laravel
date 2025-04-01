9402a99f76138d3731b480cdecc724f21b879dd2
DAVID DURAN <davidstevend@gmail.com>
2025-03-31 20:35:02 -0500

    fix: Eliminar espacio en blanco innecesario en EstablecimientoController

--- app/Http/Controllers/EstablecimientoController.php
+++ app/Http/Controllers/EstablecimientoController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Establecimiento;
-use Illuminate\Http\Request;
+use Illuminate\Http\Request;                          
 use Illuminate\Validation\ValidationException;
 
 class EstablecimientoController extends Controller
